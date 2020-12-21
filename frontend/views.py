3845d14ae18da21aeec9e03e3b0895b247d3ed99
saharun46@gmail.com <saharun46@gmail.com>
2020-12-21 12:36:44 +0000

    test again view

--- frontend/views.py
+++ frontend/views.py
@@ -5,5 +5,5 @@ from django.shortcuts import render, get_object_or_404, redirect, reverse
 # Create your views here.
 def index(request):
     print('hello mama')
-    return HttpResponse('hello mmama kemon aso?')
+    return HttpResponse('hello mmama kemon aso? ami valo asi')
 git push -u origin main
