6a5a9458975eaf07f04841c53c58017cb3a5760c
saharun46@gmail.com <saharun46@gmail.com>
2020-12-21 12:41:52 +0000

    view error fixing

--- frontend/views.py
+++ frontend/views.py
@@ -6,4 +6,3 @@ from django.shortcuts import render, get_object_or_404, redirect, reverse
 def index(request):
     print('hello mama')
     return HttpResponse('hello mmama kemon aso? ami valo asi')
-git push -u origin main
