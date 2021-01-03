840f3da62b54f44a1ccbf1b3ec63d57076765707
saharun46@gmail.com <saharun46@gmail.com>
2021-01-03 02:43:04 +0000

    create and initial index html file

--- frontend/views.py
+++ frontend/views.py
@@ -4,5 +4,7 @@ from django.shortcuts import render, get_object_or_404, redirect, reverse
 
 # Create your views here.
 def index(request):
-    print('hello mama')
-    return HttpResponse('hello mmama kemon aso? ami valo asi')
+    context = {
+        'title':'Home page'
+    }
+    return render(request, "frontend/index.html", context)
